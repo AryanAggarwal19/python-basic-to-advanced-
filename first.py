718a4aa52899bf110bdf211d0ace74030bbaed46
Aryan Agarwal <aryanaggarwal1901@gmail.com>
2024-03-17 09:19:33 -0700

    01_basics

---
+++ first.py
@@ -0,0 +1,120 @@
+#first program
+
+# print("Hello world")
+
+# print("pankhuri")
+
+# print("Aryan is my name",",","my age is 22")
+# print(23)
+# print(12+19)
+
+# declaring variables
+
+# name= "Aryan"
+# age= "22"
+# price= 25.99
+# print(name,age, price), ek he line mein print krana hai toh "," use kro
+
+# print("My name is :",name)
+# print("My age is :", age)
+
+# age2= age
+# print("printing age2 :", age2)
+
+# age= 30
+# print("My updatedAge is :", age)
+
+# type 
+# print(type(name))
+# print(type(age))
+
+
+# datatypes in python
+# integer
+# string
+# float
+# Boolean
+# none
+
+# a= 23
+# old= False
+# a= None
+
+# print(type(a),type(old))
+
+# print sum of 2 numbers
+
+# a= 23
+# b= 19
+# printSum= a+b
+
+# print("The required sum is :", printSum)
+
+# types of operators in python
+## arithmetic operator
+
+# print(not True) , return opposite answer
+
+#  type conversion in python
+
+# a= 2
+# b= 4.25
+
+# sum= a + b
+
+# print(sum)
+
+#type casting
+# a= int("2") #python mein str
+# b= 4.25
+# print(a+b)
+
+# name= int("3")
+
+
+# taking input in python 
+# function use=> input
+
+# name= int(input())
+
+# print("welcome",type(name), name)
+
+# name= input("Enter name:")
+# age= int(input("Enter age: "))
+# marks=float(input("Enter marks: "))
+
+# print(type(age))
+
+# print(name, age, marks)
+
+
+###########################################################
+#practice questions
+
+#1 sum of 2 numbers
+
+# num1= int(input("Enter num1: "))
+# num2= int(input("Enter num2: "))
+# sum= num1+num2
+
+# print("the sum of 2 numbers is:", sum)
+
+#2 area of a square
+# a= int(input("Enter the side:"))
+# Area= a **2
+# print("the area of square will be:", Area)
+
+# print avg
+# num1= float(input())
+# num2= float(input())
+# print("the avg of 2 nums will be", (num1+num2)/2)
+
+# comparison
+
+a= int(input("Enter first number: "))
+b= int(input("Enter second number:"))
+
+# print True if a>=b else print false
+print(a>=b)
+
+
